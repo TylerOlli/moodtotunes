b7904ba1e421d46e85a8635bcc2dd7324c8c03a5
Tyler Olli <ollityler@gmail.com>
2023-08-29 20:40:01 -0400

    Created stream component

--- src/app/components/stream.tsx
+++ src/app/components/stream.tsx
@@ -1,9 +1,24 @@
-export default function Stream() {
+import "./stream.css";
+
+interface Track {
+  title: string;
+  id: number;
+}
+
+interface StreamProps {
+  tracks: Track[];
+}
+
+export default function Stream({ tracks }: StreamProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
+        {tracks.map((track: Track) => (
+          <div className="track" key={track.id}>
+            {track.title}
+          </div>
+        ))}
       </div>
     </main>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
+import Stream from '@/app/components/stream'
 
-const tracks = [
-  {
-    title: 'Some track'
-  },
-  {
-    title: 'Some other track'
-  }
-];
+interface Track {
+  title: string;
+  id: number;
+}
 
 export default function Home() {
+  const tracks: Track[] = [
+    { title: "Track 1", id: 1 },
+    { title: "Track 2", id: 2 },
+    { title: "Track 3", id: 3 },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -23,11 +26,7 @@ export default function Home() {
         />
       </div>
       <div className="tracklist">
-        {
-          tracks.map((track, key) => {
-              return <div className="track" key={key}>{track.title}</div>;
-          })
-        }
+        <Stream tracks={tracks} />
       </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
