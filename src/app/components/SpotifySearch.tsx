12b389a1eb3b5c0af048d77d38e65e3ff8c45b38
Tyler Olli <ollityler@gmail.com>
2023-10-20 20:56:14 -0400

    Added ability to clear search

--- src/app/components/SpotifySearch.tsx
+++ src/app/components/SpotifySearch.tsx
@@ -1,22 +1,23 @@
 import { SearchResults, SpotifyApi } from "@spotify/web-api-ts-sdk";
-import { JSXElementConstructor, Key, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   sdk: SpotifyApi;
 }
 
 export default function SpotifySearch({ sdk }: Props) {
+  const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState<SearchResults>({} as SearchResults);
+  const [showTable, setShowTable] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      const results = await sdk.search("The Beatles", ["artist"]);
-      setResults(() => results);
-    })();
-  }, [sdk]);
+    if (searchTerm) {
+      searchSpotify(searchTerm, sdk, setResults);
+    }
+  }, [sdk, searchTerm]);
 
   // generate a table for the results
-  const tableRows = results.artists?.items.map((artist: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; popularity: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; followers: { total: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; }; }) => {
+  const tableRows = results.artists?.items.map((artist) => {
     return (
       <tr key={artist.id}>
         <td>{artist.name}</td>
@@ -28,17 +29,38 @@ export default function SpotifySearch({ sdk }: Props) {
 
   return (
     <>
-      <h1>Spotify Search for The Beatles</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Followers</th>
-          </tr>
-        </thead>
-        <tbody>{tableRows}</tbody>
-      </table>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (searchTerm) {
+          searchSpotify(searchTerm, sdk, setResults);
+          setShowTable(true);
+        }
+      }}>
+        <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <button type="submit">Search</button>
+        <button type="button" onClick={() => {
+          setSearchTerm("");
+          setResults({} as SearchResults);
+          setShowTable(false);
+        }}>Clear</button>
+      </form>
+      {showTable && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Followers</th>
+            </tr>
+          </thead>
+          <tbody>{tableRows}</tbody>
+        </table>
+      )}
     </>
   );
+}
+
+async function searchSpotify(searchTerm: string, sdk: SpotifyApi, setResults: React.Dispatch<React.SetStateAction<SearchResults>>) {
+  const results = await sdk.search(searchTerm, ["artist"]);
+  setResults(() => results);
 }
